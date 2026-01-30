fdd266ad165419d875218d4ec033623d43396499
Bsuchot <bastien.suchot@gmail.com>
2026-01-30 13:45:26 +0100

    add security controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,17 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{Response, JsonResponse, Request};
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
+
+
+#[Route('/api', name: 'app_api_')]
 final class SecurityController extends AbstractController
 {
-    #[Route('/security', name: 'app_security')]
-    public function index(): Response
+    public function __construct(
+        private SerializerInterface $serializer,
+        private EntityManagerInterface $manager,
+        )
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+    }
+
+    #[Route('/registration', name: 'registration', methods: 'POST')]
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
+    {
+        $user = $this->serializer->deserialize($request->getContent(), User::class, 'json');
+        $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+        $user->setCreatedAt(new \DatetimeImmutable());
+
+        $this->manager->persist($user);
+        $this->manager->flush();
+
+        return new JsonResponse(
+            ['user' => $user->getUserIdentifier(),
+            'apiToken' => $user->getApiToken(),
+            'roles' => $user->getRoles()
+            ],
+            Response::HTTP_CREATED
+        );
+    }
+
+    #[Route('/login', name: 'login', methods: 'POST')]
+    public function login(#[CurrentUser] ?User $user): JsonResponse
+    {
+        if (null === $user) {
+            return new JsonResponse(['message' => 'Missing credentials'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return new JsonResponse([
+            'user'  => $user->getUserIdentifier(),
+            'apiToken' => $user->getApiToken(),
+            'roles' => $user->getRoles(),
         ]);
     }
 }
